71bf4a2fda8d28754d6ebdc35f5c1e4b4d554678
MATHESH-45 <mathesh4513@gmail.com>
2024-04-21 16:49:49 +0530

    refactor

--- day3typeconversionmathesh/type.py
+++ day3typeconversionmathesh/type.py
@@ -1,5 +1,5 @@
-a=6    #declared a variable a , type=int
-b=9.9  #declared a variable b, type=float
-b=int(b) #converting variable b from float to int
-print(b) #printing b to check whether the float data type gets converted to int
-print(a+b) #adding two numbers 
+num=6    #declared a variable a , type=int
+float_=9.9  #declared a variable b, type=float
+float_=int(float_) #converting variable b from float to int
+print(float_) #printing b to check whether the float data type gets converted to int
+print(num+float_) #adding two numbers 
