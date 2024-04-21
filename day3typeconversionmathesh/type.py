36c6be9a1fc829f0291e4d93ee1a141efd17a93b
MATHESH-45 <mathesh4513@gmail.com>
2024-04-21 16:42:26 +0530

    typeconversionex

---
+++ day3typeconversionmathesh/type.py
@@ -0,0 +1,5 @@
+a=6    #declared a variable a , type=int
+b=9.9  #declared a variable b, type=float
+b=int(b) #converting variable b from float to int
+print(b) #printing b to check whether the float data type gets converted to int
+print(a+b) #adding two numbers 
