32913a88203690805e45e72c9df20ee0c984716f
sahithiya007 <sahithiya007@gmail.com>
2024-04-21 16:48:52 +0530

    refactor

--- day3typecon/type.py
+++ day3typecon/type.py
@@ -1,4 +1,4 @@
-num_float = 12#checked float
+num_float = 12.5#checked float
 num_int = 23#checked int
-print("datatype of nuum_float before type casting",type(num_float))#checked datatype
+print("datatype of nuum_float before type casting",type(num_int))#checked datatype
 num_float= int(num_float)#checked number
