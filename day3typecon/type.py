1320277edc96d63e7ce49d4d7e722eced70ca5ff
sahithiya007 <sahithiya007@gmail.com>
2024-04-21 16:34:26 +0530

    adding prog for typecon

---
+++ day3typecon/type.py
@@ -0,0 +1,4 @@
+num_float = 12#checked float
+num_int = 23#checked int
+print("datatype of nuum_float before type casting",type(num_float))#checked datatype
+num_float= int(num_float)#checked number
