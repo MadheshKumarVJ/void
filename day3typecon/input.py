3d83aa238e5df9ce174a7b3bb37104156d06f12d
sahithiya007 <167623435+sahithiya007@users.noreply.github.com>
2024-04-21 17:34:07 +0530

    add program to get user details (#6)

---
+++ day3typecon/input.py
@@ -0,0 +1,4 @@
+name=input("enter your name is")#entering name
+age=float(input("enter your age is"))#entering age
+phonenumber=int(input("enter your phonenumber is"))#entering phonenumber
+print('the value of name is{} and age is {} and phonenumber is{}'.format(name,age,phonenumber))#input
