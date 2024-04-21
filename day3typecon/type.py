a6797c84063dffc8c1c73efeb6753558d3b494f3
sahithiya007 <sahithiya007@gmail.com>
2024-04-21 16:55:59 +0530

    refactory

--- day3typecon/type.py
+++ day3typecon/type.py
@@ -1,4 +1,3 @@
 num_float = 12.5#checked float
-num_int = 23#checked int
+num_int = int(num_float) #changing float to int 
 print("datatype of nuum_float before type casting",type(num_int))#checked datatype
-num_float= int(num_float)#checked number
