37dc6abfc72e071e3b8a0e8458ccee4940b99dc8
MATHESH-45 <mathesh4513@gmail.com>
2024-04-21 17:26:27 +0530

    getuserdata

---
+++ day3typeconversionmathesh/input.py
@@ -0,0 +1,4 @@
+name=input("enter your name:")
+age=float(input("enter your age:"))
+number=int(input("enter your phone number:"))
+print("your name is {} and your age is {} and your phone number is {}".format(name,age,number))
