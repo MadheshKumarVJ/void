76560a3a8e549f9871f8b3b4e604a194a23b6e32
MATHESH-45 <167112985+MATHESH-45@users.noreply.github.com>
2024-04-21 16:53:00 +0530

    Adding program for typeconversion (#4)

---
+++ day3typeconversionmathesh/type.py
@@ -0,0 +1,5 @@
+num=6    #declared a variable a , type=int
+float_=9.9  #declared a variable b, type=float
+float_=int(float_) #converting variable b from float to int
+print(float_) #printing b to check whether the float data type gets converted to int
+print(num+float_) #adding two numbers 
