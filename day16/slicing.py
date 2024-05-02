85eb0f4486f479b658d9018f7c488d96cc03f3a7
MATHESH-45 <mathesh4513@gmail.com>
2024-05-02 22:41:01 +0530

    Slicing

---
+++ day16/slicing.py
@@ -0,0 +1,25 @@
+#slicing
+
+#string
+str='learning'
+print(str[:])  #learning
+print(str[2:5]) #arn
+
+#list
+lst=[1,2,'a','py',3]
+print(lst[:3])  #[1,2,'a']
+print(lst[2:])  #['a','py',3]
+print(lst[1:-1]) #[2,'a','py']
+
+#tuple
+tup=(1,2,3,4,'ro')
+print(tup[-3:-1]) #(3,4)
+print(tup[-1:]) #('ro',)
+print(tup[:-3]) #(1,2)
+
+#dictionary cannot perform slicing
+
+
+
+#set cannot perform slicing
+
